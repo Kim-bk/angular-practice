c5a7ca951f5d1eb06b40a3924bf7f67e95460e31
Kim Nguyễn Hoàng <102190072@sv1.dut.udn.vn>
2023-01-05 17:39:44 +0700

    content projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular100day';
+  name = 'angular100day';
+  questions = {
+      question1: true,
+      question2:false,
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthorDetailsComponent } from './authors/author-details/author-details.component';
 import { AuthorListComponent } from './authors/author-list/author-list.component';
 import { HelloComponent } from './hello/hello.component';
 import { HiComponent } from './hi/hi.component';
+import { ToggleComponent } from './toggle/toggle.component';
 
 
 @NgModule({
@@ -13,13 +15,15 @@ import { HiComponent } from './hi/hi.component';
         HelloComponent,
         HiComponent,
         AuthorListComponent,
-        AuthorDetailsComponent
+        AuthorDetailsComponent,
+        ToggleComponent
 
     ],
     providers: [],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
+        FormsModule,
     ]
 })
 export class AppModule { }
